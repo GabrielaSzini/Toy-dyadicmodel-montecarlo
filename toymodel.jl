928124ae3bfd9e4b9e3254f02621af9e95afb727
NoFishLikeIan <andreatitton96@gmail.com>
2021-03-17 19:00:01 +0100

    🪐 parallel simulation

--- toymodel.jl
+++ toymodel.jl
@@ -1,8 +1,57 @@
-using Random, Distributions
-using Combinatorics
+using Distributed
+addprocs(4; exeflags="--project")
 
-include("utils/stats.jl")
-include("utils/dgp.jl")
+@everywhere begin
 
-Random.seed!(111222)
+    using Random, Distributions
+    using Combinatorics
+    using LinearAlgebra
 
+    include("utils/stats.jl")
+    include("utils/dgp.jl")
+
+end
+
+
+@everywhere function simulation(N)
+
+    E = Set(1:N)
+
+    X, U = datageneration(N)
+
+    tetrads = permutations(1:N, 4)
+
+    ΔX, ΔU = Δfe(X), Δfe(U)
+    Ustat = mean(@. ΔX(tetrads) * ΔU(tetrads))
+    
+    # Variance
+    Ndyads = N * (N - 1)
+    s̄ = zeros(Ndyads)
+
+    for (l, dyad) in enumerate(permutations(1:N, 2))
+
+        i, j = dyad
+
+        # Combinations of not i, j dyads
+        notij = collect(setdiff(E, Set(dyad)))
+        othercomb = combinations(notij, 2)
+
+        s̄[l] = mean(scombs(X, U, i, j, k, l) for (k, l) in othercomb)
+        
+    end
+
+    Δ₂ = mean(s̄.^2)
+
+    return Δ₂, Ustat 
+
+end
+
+N = 40
+sims = 10
+
+function parallelrun()
+    tomap = [N for _ in 1:sims]
+    return pmap(simulation, tomap)
+end
+
+@time result = parallelrun() 
