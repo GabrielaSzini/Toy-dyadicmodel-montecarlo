bf441e81d9c92a80e2fa5be7ac794eef6a450376
NoFishLikeIan <andreatitton96@gmail.com>
2021-03-19 11:42:26 +0100

    🧹 moved Delta to new file

--- simulation.jl
+++ simulation.jl
@@ -15,40 +15,8 @@ function simulation(N)
 
     X, U = datageneration(N)
 
-    @time Ustat = computeU(X, U, N)
-        
-    # Obtaining Δ₂
-    Ndyads = N * (N - 1)
-    s̄₁ = 0.
-    s̄₂ = 0.
-
-    # Fixing a dyad i,j
-    @inbounds for i in 1:N, j in 1:N
-        (i - j) == 0 && continue
-
-        sdyad₁ = 0.
-        sdyad₂ = 0.
-        Ncombs = binomial(N - 2, 2)
-
-        # Obtaining the conditional expectations over this dyad
-        for l in 1:N
-            (l - i) * (l - j) == 0 && continue
-
-            for k in l:N
-                (k - i) * (k - j) == 0 && continue
-
-                sdyad₁ += scombs(X, U, i, j, k, l)
-                sdyad₂ += scombsinefficient(X, U, i, j, k, l) 
-            end
-        end
-
-        s̄₁ += (sdyad₁ / Ncombs)^2
-        s̄₂ += (sdyad₂ / Ncombs)^2
-    end
-
-    # Taking the average over dyads for both estimators
-    Δ₂₁ = s̄₁ / Ndyads
-    Δ₂₂ = s̄₂ / Ndyads 
+    Ustat = computeU(X, U, N)
+    Δ₂₁, Δ₂₂ = computeΔ₂(X, U, N)
 
     return Δ₂₁, Δ₂₂, Ustat 
 
--- utils/stats.jl
+++ utils/stats.jl
@@ -68,3 +68,42 @@ end
 
 end
 
+
+
+@inline function computeΔ₂(X::Matrix, U::Matrix, N::Int64)
+
+    Ndyads = N * (N - 1)
+    s̄₁ = 0.
+    s̄₂ = 0.
+
+    # Fixing a dyad i,j
+    @inbounds for i in 1:N, j in 1:N
+        (i - j) == 0 && continue
+
+        sdyad₁ = 0.
+        sdyad₂ = 0.
+        Ncombs = binomial(N - 2, 2)
+
+        # Obtaining the conditional expectations over this dyad
+        for l in 1:N
+            (l - i) * (l - j) == 0 && continue
+
+            for k in l:N
+                (k - i) * (k - j) == 0 && continue
+
+                sdyad₁ += scombs(X, U, i, j, k, l)
+                sdyad₂ += scombsinefficient(X, U, i, j, k, l) 
+            end
+        end
+
+        s̄₁ += (sdyad₁ / Ncombs)^2
+        s̄₂ += (sdyad₂ / Ncombs)^2
+    end
+
+    # Taking the average over dyads for both estimators
+    Δ₂₁ = s̄₁ / Ndyads
+    Δ₂₂ = s̄₂ / Ndyads 
+
+    return Δ₂₁, Δ₂₂
+
+end
