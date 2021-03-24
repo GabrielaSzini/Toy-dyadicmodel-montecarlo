7df4f4e819f865ec7af37d8999c2e9274c4fd030
Gabriela Szini <62068248+GabrielaSzini@users.noreply.github.com>
2021-03-24 15:04:34 +0100

    Plotting histograms

--- plot.jl
+++ plot.jl
@@ -3,10 +3,13 @@ using Plots
 using CSV
 using DataFrames
 
-result = DataFrame(CSV.File("results/outN50sims10000.csv", header=0))
+result = DataFrame(CSV.File("results/outN10sims1000.csv", header=0))
 """ 
 Some further results to obtain the factors and plots
 """
+N=10
+sims=10000
+
 Δ₂₁ = result[:,1]
 Δ₂₂ = result[:,2]
 Ustat = result[:,3]
@@ -18,10 +21,10 @@ F̄₁ = varUstat * (N * (N - 1)) / mean(Δ₂₁)
 @printf("For N = %.0f, and S = %.0f, meanF₁ = %0.5f", N, sims, float(meanF₁))
 @printf("For N = %.0f, and S = %.0f, F̄₁ = %0.5f", N, sims, float(F̄₁))
 
-hist₁ = histogram(F₁, bins=:scott, title="For N = 40, and S = 10", label="", xlabel="F₁",
+hist₁ = histogram(F₁, bins=:scott, title="For N = 10, and S = 1000", label="", xlabel="F₁",
 ylabel="Frequency")
 hist₁
-savefig("results/histF1_N40_S10.pdf")
+savefig("results/histF1_N10_S1000.pdf")
 
 F₂ = varUstat * (N * (N - 1)) ./ Δ₂₂
 meanF₂ = mean(F₂)
@@ -29,7 +32,7 @@ F̄₂ = varUstat * (N * (N - 1)) / mean(Δ₂₂)
 @printf("For N = %.0f, and S = %.0f, meanF₂ = %0.5f", N, sims, float(meanF₂))
 @printf("For N = %.0f, and S = %.0f, F̄₂ = %0.5f", N, sims, float(F̄₂))
 
-hist₂ = histogram(F₂, bins=:scott, title="For N = 40, and S = 10", label="", xlabel="F₂",
+hist₂ = histogram(F₂, bins=:scott, title="For N = 10, and S = 1000", label="", xlabel="F₂",
 ylabel="Frequency")
 hist₂
-savefig("results/histF2_N40_S10.pdf")
+savefig("results/histF2_N10_S1000.pdf")
